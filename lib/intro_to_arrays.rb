7940ef7acf59339e821bf5ec3380f3096523a0f8
Daniel Ran <danielr1735@gmail.com>
2018-06-19 10:39:12 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,23 @@ end
 def array_with_two_elements
   @my_new_array = ['a', 'b']
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.shift
+end
+
+def last_element_with_array_methods(array)
+  array.pop
+end
